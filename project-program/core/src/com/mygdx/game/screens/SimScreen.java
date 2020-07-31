301984592cf61c1c86b0f8ce29006b71cf5b8c1f
shalamike <mshala02@dcs.bbk.ac.uk>
2020-07-31 05:07:09 +0100

    now the simscreen actually renders the bad logic jpg

--- project-program/core/src/com/mygdx/game/screens/SimScreen.java
+++ project-program/core/src/com/mygdx/game/screens/SimScreen.java
@@ -1,8 +1,13 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGdxGame;
@@ -10,19 +15,21 @@ import com.mygdx.game.MyGdxGame;
 
 /* this screen is where the main simulation of the bouncing bomb will take place*/
 public class SimScreen implements Screen {
-    private MyGdxGame sim; //
-
+    private MyGdxGame sim;
+    // creating the camera and view port
     private OrthographicCamera simCam;
     private Viewport simPort;
+
     private Texture texture;
 
     public SimScreen(MyGdxGame sim){
         this.sim = sim;
 
         simCam = new OrthographicCamera();
-        simPort = new ScreenViewport();
+        simPort = new ScreenViewport(simCam);
 
-        texture = new Texture("badlogic.jph");
+        texture = new Texture("badlogic.jpg");
+
     }
 
 
@@ -34,7 +41,11 @@ public class SimScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        sim.batch.begin();
+        sim.batch.draw(texture, 0, 0);
+        sim.batch.end();
     }
 
     @Override
