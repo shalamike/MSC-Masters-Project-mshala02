b2e6f0c2645ce82781ce67ce19407d375f3e0cf5
shalamike <mshala02@dcs.bbk.ac.uk>
2020-07-31 04:17:11 +0100

    added some base classes in java for now with some boilerplate code

---
+++ project-program/core/src/com/mygdx/game/screens/SimScreen.java
@@ -0,0 +1,50 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.MyGdxGame;
+
+public class SimScreen implements Screen {
+    private MyGdxGame game; //
+
+    private OrthographicCamera simCam;
+    private Viewport simPort;
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
