75bd0f8a86849d3d37a26d0df9088343623bf3e3
shalamike <m.shalaby1994@gmail.com>
2022-07-18 22:42:57 +0100

    fixed the hud and added additional water behind the dam

--- project-program/core/src/com/mygdx/game/scenes/Hud.java
+++ project-program/core/src/com/mygdx/game/scenes/Hud.java
@@ -21,74 +21,45 @@ public class Hud {
     // this is to ensure that the hud stays in place while the game moves
     public Viewport viewport;
 
-
-
-    private Integer worldTimer;
-    private float timeCount;
-    private Integer score;
+    private Integer distanceToDam;
+    private Integer rpm;
+    private Integer speed;
     private static float scale = 1;
 
-    Label distanceLabel;
-    Label speedLabel;
-    Label bombRPMlabel;
-    Label distance;
-    Label worldLabel;
-    Label marioLabel;
+    Label rpmIntLabel;
+    Label rpmTextLabel;
+    Label speedIntLabel;
+    Label speedTextLabel;
+    Label distanceToDamIntLabel;
+    Label distanceToDamTextLabel;
 
-    public Hud(SpriteBatch sb){
-        worldTimer = 300;
-        timeCount = 0;
-        score = 0;
+    public Hud (SpriteBatch sb){
+        distanceToDam = 5;
+        rpm = 720;
+        speed = 200;
+
         viewport = new FitViewport(PhysicsImp.S_WIDTH, PhysicsImp.S_HEIGHT, new OrthographicCamera());
-        // a stage is kind of like an empty box
-        stage = new Stage(viewport, sb);
-
-        // therefore what we need is a table to organise everything inside our empty box
+        stage =  new Stage (viewport, sb);
         Table table = new Table();
-        //by efault the table will be in the centre of our stage
         table.top();
-        // now the table is on top of the stage
         table.setFillParent(true);
-        // now the table is the size of the stage
 
-        //time to create the labels
-        distanceLabel = new Label(String.format("Distance from Dam (Miles)"), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
-        speedLabel = new Label(String.format("Bomb's speed", score), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
-        bombRPMlabel = new Label("Bomb RPM", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
-        distance = new Label("", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
-        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
-        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        rpmTextLabel = new Label("Bomb's Rotation Speed (RPM)", new Label.LabelStyle(new BitmapFont(), Color.CORAL)) ;
+        rpmIntLabel = new Label(String.format("%06d", rpm), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        speedTextLabel = new Label("current speed of bomb (MPH)", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        speedIntLabel = new Label (String.format("%03d", speed), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        distanceToDamTextLabel = new Label("Distancfe to dam (miles)", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        distanceToDamIntLabel = new Label(String.format("%03d", distanceToDam), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
 
-        distanceLabel.setFontScale(scale);
-        speedLabel.setFontScale(scale);
-        bombRPMlabel.setFontScale(scale);
-        distance.setFontScale(scale);
-        worldLabel.setFontScale(scale);
-        marioLabel.setFontScale(scale);
-
-//        Group group = new Group();
-//        group.addActor(countDownLabel);
-//        group.addActor(scoreLabel);
-//        group.addActor(timeLabel);
-//        group.addActor(levelLabel);
-//        group.addActor(worldLabel);
-//        group.addActor(marioLabel);
-//        group.setScale(1/16f, 1/16f);
-
-
-
-        // now time to add the labels to our table
-        // expandX expands the label the entire width of our screen. if multiple items use expandX, then the screen will share those equally by default
-        // pad Top will set the spacing between the top of the screen and our label by 10px in this case
-        table.add(marioLabel).expandX().padTop(10);
-        table.add(worldLabel).expandX().padTop(10);
-        table.add(bombRPMlabel).expandX().padTop(10);
+        table.add(speedTextLabel).expandX().padTop(10);
+        table.add(rpmTextLabel).expandX().padTop(10);
+        table.add(distanceToDamTextLabel).expandX().padTop(10);
         table.row();
-        table.add(speedLabel).expandX();
-        table.add(distance).expandX();
-        table.add(distanceLabel).expandX();
+        table.add(speedIntLabel).expandX();
+        table.add(rpmIntLabel).expandX();
+        table.add(distanceToDamIntLabel).expandX();
 
         stage.addActor(table);
-
     }
+
 }
