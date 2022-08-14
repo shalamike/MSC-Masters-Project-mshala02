07479eeeedc33b4584e89ad03414d0f16cb7d783
shalamike <m.shalaby1994@gmail.com>
2022-08-14 22:53:48 +0100

    bomb created

--- project-program/core/src/com/mygdx/game/sprites/Bomb.java
+++ project-program/core/src/com/mygdx/game/sprites/Bomb.java
@@ -1,12 +1,13 @@
 package com.mygdx.game.sprites;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class Bomb {
+public class Bomb extends Sprite {
     public static final float radius = 12.5f; // will make it private with getters and setters once i create the menu screen
     public static final float bombDensity = 3.6f; // will make it private variable with getters and setters once i create the menu screen
     public static final float RPM = 500;
@@ -31,7 +32,7 @@ public class Bomb {
 
     public void defineBomb(){
         BodyDef bdef = new BodyDef(); // creating a new body definition for the bomb
-        bdef.position.set(800, 550); // temporarily setting bomb position
+        bdef.position.set(80, 550); // temporarily setting bomb position
 
         bdef.type = BodyDef.BodyType.DynamicBody; // setting the bombs body to dynamic body
         b2dbody = world.createBody(bdef);//now we have the box2d body defined, we can create the body in our game world
