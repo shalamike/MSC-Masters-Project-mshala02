ce85020c19324c9576bae9b05d39d5a8f9ea3381
shalamike <m.shalaby1994@gmail.com>
2022-09-16 04:24:32 +0100

    added an animation for the bomb that doesnt seem to work very well

--- project-program2/core/src/com/mygdx/game/sprites/Bomb.java
+++ project-program2/core/src/com/mygdx/game/sprites/Bomb.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.PhysicsImp;
 import com.mygdx.game.screens.SimScreen;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -21,6 +22,15 @@ public class Bomb extends Sprite{
     public World world; // this will be the world that mario will live inside
     public Body b2dbody; // box2d body
 
+    //creating states depending on whether or not the bomb is sinking or bouncing
+    public enum State{BOUNCING, SINKING}
+    public State currentState; // creating two states to keep track of when to change states
+    public State previousState;
+    private Animation<TextureRegion> bombSpinning; //
+
+    private boolean isSpinning;
+
+    private float stateTimer;
 
     private BodyDef bdef;
     private FixtureDef fdef;
@@ -31,6 +41,17 @@ public class Bomb extends Sprite{
     public Bomb(World world, SimScreen screen){
         super(screen.getAtlas().findRegion("circleRotation0"));
 
+        currentState = State.BOUNCING;
+        previousState = State.BOUNCING;
+        stateTimer = 0;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>(); //creating an array for our frames to animate
+        for (int i =0 ; i<3 ; i++)
+            frames.add(new TextureRegion(getTexture(), i * 439, 0, 439,439));  //creating a for loop to add the appropriate images to our animation
+        bombSpinning = new Animation<TextureRegion>(0.1f, frames); // animating our frames like a flip book with each one lasting 0.1f will change according to rpm
+        frames.clear();
+
+
         bdef = new BodyDef(); // creating a new body definition for the bomb
         fdef = new FixtureDef(); // creating a new fixture def
         shape = new CircleShape(); // creating a circle for our fixture def for now
@@ -38,15 +59,44 @@ public class Bomb extends Sprite{
         this.world = world;
         defineBomb();
 
-        stationaryBomb = new TextureRegion(getTexture(), 0,0,439, 424);
+        stationaryBomb = new TextureRegion(getTexture(), 0,0,439, 439);
 
         setBounds(0,0, PhysicsImp.RADIUS * 2 /PhysicsImp.UNITSCALE, PhysicsImp.RADIUS * 2 /PhysicsImp.UNITSCALE);
 
         setRegion(stationaryBomb);
     }
 
+    //checking to see if the bombs state is sinking or bouncing
+    public State getState(){
+        if(b2dbody.getLinearVelocity().x == 0 ) //if bombs velocity is 0 then it must be sinking
+            return State.SINKING;
+        else
+            return State.BOUNCING;
+    }
+
+    public TextureRegion getFrame(float dt){
+        currentState = getState();
+
+        TextureRegion region;
+        switch (currentState){
+            case SINKING:
+                region = stationaryBomb;
+                break;
+            case BOUNCING:  //setting the bouncing case as the Default case
+            default:
+                region = bombSpinning.getKeyFrame(stateTimer);
+                break;
+
+        }
+
+        stateTimer = currentState == previousState ? stateTimer + dt : 0; // setting our state to either current state and checking to see if states have changed so that we reset our state timer
+        previousState = currentState;
+        return region;
+    }
+
     public void update(float dt){
         setPosition(b2dbody.getPosition().x - getWidth() / 2 , b2dbody.getPosition().y - getHeight() /2);
+        setRegion(getFrame(dt));
 //        if (PhysicsImp.BOMB_EXPLODES){
 //            fdef.filter.categoryBits = PhysicsImp.DESTROYED_BIT;
 //
